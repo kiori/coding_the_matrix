72404e5a210fc748a2e2eccf68b8f5c54f4cea18
Kevin <kiori@counsyl.com>
2013-07-20 23:03:39 -0700

    adding hw1

--- hw/hw0/hw0.py
+++ hw/hw0/hw0.py
@@ -5,55 +5,77 @@
 
 
 ## Problem 1
-def myFilter(L, num): pass
+def myFilter(L, num): return [xx for xx in L if xx%num!=0]
 
 
 
 ## Problem 2
-def myLists(L): pass
+def myLists(L): return [list(range(1, (xx+1))) for xx in L]
 
 
 
 ## Problem 3
-def myFunctionComposition(f, g): pass
+def myFunctionComposition(f, g): return {key:g[value] for key,value in f.items()}
 
 
 ## Problem 4
 # Please only enter your numerical solution.
 
-complex_addition_a = ... 
-complex_addition_b = ...
-complex_addition_c = ...
-complex_addition_d = ...
+complex_addition_a = (5+3j)
+complex_addition_b = 1j
+complex_addition_c = (-1+0.001j)
+complex_addition_d = (0.001+9j)
 
 
 
 ## Problem 5
-GF2_sum_1 = ...
-GF2_sum_2 = ...
-GF2_sum_3 = ...
+GF2_sum_1 = 1
+GF2_sum_2 = 0
+GF2_sum_3 = 0
 
 
 ## Problem 6
-def mySum(L): pass
-
+def mySum(L):
+	current = 0
+	if len(L)!=0:
+		for number in L:
+			current += number
+	return current
 
 
 ## Problem 7
-def myProduct(L): pass
+def myProduct(L):
+	if len(L)!=0:
+		current = 1
+		for number in L:
+			current *= number
+	else:
+		current = 0 
+
+	return current
 
 
 
 ## Problem 8
-def myMin(L): pass
+def myMin(L):
+	return sorted(L).pop(0)
 
 
 
 ## Problem 9
-def myConcat(L): pass
+def myConcat(L):
+	current = ''
+	for word in L:
+		if type(word) is str:
+			current += word
 
+	return current
 
 
 ## Problem 10
-def myUnion(L): pass
+def myUnion(L):
+	current = set()
+	for item in L:
+		current.update(item)
+	return current
 
---
+++ hw/hw1/scratch.py
@@ -0,0 +1,43 @@
+from GF2 import one
+import numpy as np
+from itertools import chain, combinations
+
+def problems_4_and_5(problem):
+
+
+u_answer = np.array([0, 0 , one, 0, 0, one,0 ])
+v_answer = np.array([0, one , 0, 0, 0, one , 0])
+
+if problem==4:
+	dict_str = {'a':'1100000','b':'0110000','c':'0011000','d':'0001100','e':'0000110','f':'0000011'}
+if problem==5:
+dict_str = {'a':'1110000','b':'0111000','c':'0011100','d':'0001110','e':'0000111','f':'0000011'}
+
+def change_dict(value):
+	new_value = []
+	for char in value:
+		if char=='1':
+			new_value.append(one)
+		else:
+			new_value.append(0)
+	return np.array(new_value)
+
+dict_array = {key:change_dict(value) for key, value in dict_str.iteritems()}
+key_list = dict_array.keys()
+
+combs = []
+for i in xrange(2, len(key_list)+1):
+    els = [list(x) for x in combinations(key_list, i)]
+    combs.append(els)
+
+flat_combs = [val for subl in combs for val in subl]
+
+for index, vectors in enumerate(flat_combs):
+	vector_sum=dict_array[vectors[0]]
+	for vector in vectors[1:]:
+		vector_sum = vector_sum + dict_array[vector]
+	print index, " end: ", vector_sum	
+	if (vector_sum==u_answer).all():
+		print "U-Answer Vector: ", sorted(vectors)
+	if (vector_sum==v_answer).all():
+		print "V-Answer Vector: ", sorted(vectors)
